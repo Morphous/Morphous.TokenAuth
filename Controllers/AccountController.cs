3159a421a77a78aa18431295a09f92d20692cbad
Tom Pratt <tinglybill@outlook.com>
2015-05-21 17:29:27 +0100

    adding middleware and account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HttpAuth.Controllers {
+    public class AccountController : ApiController {
+        public string Get() {
+
+            return "test";
+        }
+    }
+}
---
+++ Owin/AuthMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Orchard.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HttpAuth.Owin {
+    public class AuthMiddleware : IOwinMiddlewareProvider {
+
+        public IEnumerable<OwinMiddlewareRegistration> GetOwinMiddlewares() {
+            return new[] {
+                new OwinMiddlewareRegistration {
+                    Priority = "1",
+                    Configure = app => {
+                        var oAuthOptions = new OAuthAuthorizationServerOptions
+                        {
+                            TokenEndpointPath = new PathString("/Token"),
+                        //    Provider = new ApplicationOAuthProvider(PublicClientId),
+                        //    AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                            AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                            AllowInsecureHttp = true
+                        };
+
+                        // Enable the application to use bearer tokens to authenticate users
+                        app.UseOAuthAuthorizationServer(oAuthOptions);
+                        app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+                        
+                    }
+                }
+            };
+        }
+    }
+}
