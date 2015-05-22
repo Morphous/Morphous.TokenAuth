22e5ea849ca2d43290073cd8e554b5a3c7d2a4ab
Tom Pratt <tinglybill@outlook.com>
2015-05-22 18:39:29 +0100

    Get IMembershipService in provider using Work<T>
    Check if valid user before issuing token

--- Owin/AuthMiddleware.cs
+++ Owin/AuthMiddleware.cs
@@ -1,7 +1,9 @@
 ﻿using HttpAuth.Providers;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
+using Orchard.Environment;
 using Orchard.Owin;
+using Orchard.Security;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,11 @@ using System.Web;
 
 namespace HttpAuth.Owin {
     public class AuthMiddleware : IOwinMiddlewareProvider {
+        private readonly Work<IMembershipService> _membershipServiceWork;
+
+        public AuthMiddleware(Work<IMembershipService> membershipServiceWork) {
+            _membershipServiceWork = membershipServiceWork;
+        }
 
         public IEnumerable<OwinMiddlewareRegistration> GetOwinMiddlewares() {
             return new[] {
@@ -19,7 +26,7 @@ namespace HttpAuth.Owin {
                         var oAuthOptions = new OAuthAuthorizationServerOptions
                         {
                             TokenEndpointPath = new PathString("/Token"),
-                            Provider = new AuthProvider(),
+                            Provider = new AuthProvider(_membershipServiceWork),
                         //    AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
                             AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
                             AllowInsecureHttp = true
@@ -27,8 +34,7 @@ namespace HttpAuth.Owin {
 
                         // Enable the application to use bearer tokens to authenticate users
                         app.UseOAuthAuthorizationServer(oAuthOptions);
-                        app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
-                        
+                        app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());                        
                     }
                 }
             };
--- Providers/AuthProvider.cs
+++ Providers/AuthProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Owin.Security.OAuth;
+using Orchard.Environment;
+using Orchard.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,35 +10,31 @@ using System.Web;
 
 namespace HttpAuth.Providers {
     public class AuthProvider : OAuthAuthorizationServerProvider {
+        private readonly Work<IMembershipService> _membershipServiceWork;
+
+        public AuthProvider(Work<IMembershipService> membershipServiceWork) {
+            _membershipServiceWork = membershipServiceWork;
+        }
+
         public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context) {
             context.Validated();
             return Task.FromResult<object>(null);
         }
 
         public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context) {
-
             context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
 
-            var name = context.UserName;
-            var pass = context.Password;
-            /*
-            using (AuthRepository _repo = new AuthRepository()) {
-                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+            var membershipService = _membershipServiceWork.Value;
 
-                if (user == null) {
-                    context.SetError("invalid_grant", "The user name or password is incorrect.");
-                    return;
-                }
+            var user = membershipService.ValidateUser(context.UserName, context.Password);
+            if (user == null) {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+            } else {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+           //     identity.AddClaim(new Claim("role", "user"));
+                context.Validated(identity);
             }
-             * */
-
-         //   return Task.FromResult<object>(null);
-
-            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-            identity.AddClaim(new Claim("sub", context.UserName));
-            identity.AddClaim(new Claim("role", "user"));
-
-            context.Validated(identity);
+            
             return Task.FromResult<object>(null);
         }
     }
